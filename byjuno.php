3b3023dd27c007c3e286408758e9734ec3fcd976
test <test@test.com>
2018-12-02 13:08:47 +0200

    test

--- byjuno.php
+++ byjuno.php
@@ -16,7 +16,7 @@ class Byjuno extends PaymentModule
         $this->name = 'byjuno';
         $this->tab = 'payments_gateways';
         $this->version = '1.0.0';
-        $this->author = 'Byjuno.ch (http://www.byjuno.ch/)';
+        $this->author = 'Byjuno.ch';
         $this->controllers = array('payment', 'validation', 'errorpayment');
         $this->is_eu_compatible = 1;
         $this->bootstrap = true;
