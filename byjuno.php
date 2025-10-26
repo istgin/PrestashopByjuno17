1c0ade861a52a68695f96d4c7b7f2cf877baf516
byjuno-dev <byjuno-dev@byjuno.ch>
2025-10-26 17:40:24 +0200

    gb & tmx

--- byjuno.php
+++ byjuno.php
@@ -878,11 +878,25 @@ class Byjuno extends PaymentModule
                 $cookie->intrumId = Context::getContext()->cookie->checksum;
             }
             echo '
-                <script type="text/javascript" src="https://h.online-metrix.net/fp/tags.js?org_id=' . Configuration::get("INTRUM_TMXORGID") . '&session_id=' . $cookie->intrumId . '&pageid=checkout"></script>
+            <script>
+                window.addEventListener("load", function () {
+                    window.setTimeout(function() {
+                        const s = document.createElement("script");
+                        s.type = "text/javascript";
+                        s.src = "https://cdn.cembrapay.ch/public/fp-clientlib-v5.js";
+                        s.async = true;
+                        s.onload = function() {
+                            threatmetrix.profile("prof4rs.cembrapay.ch", "' . Configuration::get("INTRUM_TMXORGID") . '", "' . $cookie->intrumId . '");
+                        };
+                        document.body.appendChild(s);
+                    }, 0);
+                });
+            </script>
+            <link rel="preload" href="https://cdn.cembrapay.ch/public/fp-clientlib-v5.js" as="script">
             <noscript>
-            <iframe style="width: 100px; height: 100px; border: 0; position: absolute; top: -5000px;" src="https://h.online-metrix.net/tags?org_id=' . Configuration::get("INTRUM_TMXORGID") . '&session_id=' . $cookie->intrumId . '&pageid=checkout"></iframe>
+            <iframe style="width: 100px; height: 100px; border: 0; position: absolute; top: -5000px;" src="https://prof4rs.cembrapay.ch/fp/tags?org_id=' . Configuration::get("INTRUM_TMXORGID") . '&session_id=' . $cookie->intrumId . '"></iframe>
             </noscript>
-                ';
+            ';
         }
     }
 
---
+++ translations/gb.php
@@ -0,0 +1,58 @@
+<?php
+global $_MODULE;
+$_MODULE = array();
+
+$_MODULE['<{byjuno}prestashop>byjuno_1335094cae0deb52ff6efc9b8840e8de'] = 'Pay by CembraPay invoice';
+$_MODULE['<{byjuno}prestashop>byjuno_0388a4ccc8d2065cdc94da8fba58a30a'] = 'CembraPay invoice';
+$_MODULE['<{byjuno}prestashop>byjuno_4a08d53adc4052023681df0c2a89b073'] = 'Pay by CembraPay installment';
+$_MODULE['<{byjuno}prestashop>byjuno_ee946970cb5f21c257e87b07e2122e06'] = 'CembraPay installment';
+
+$_MODULE['<{byjuno}prestashop>byjuno_88526efe38fd18179a127024aba8c1d7'] = 'Your order on %s is complete.';
+
+$_MODULE['<{byjuno}prestashop>byjuno_b2f40690858b404ed10e62bdf422c704'] = 'Amount %s';
+$_MODULE['<{byjuno}prestashop>byjuno_0f09dc266f1e577e5ac4dc3c5cb84138'] = 'Order reference %s';
+
+$_MODULE['<{byjuno}prestashop>payment_return_d15feee53d81ea16269e54d4784fa123'] = 'We noticed a problem with your order. If you think this is an error, feel free to contact our';
+$_MODULE['<{byjuno}prestashop>payment_return_66fcf4c223bbf4c7c886d4784e1f62e4'] = 'expert customer support team';
+
+$_MODULE['<{byjuno}prestashop>payment_error_639165db94bc68d5db4a0f4e11802f11'] = 'CembraPay payments';
+$_MODULE['<{byjuno}prestashop>payment_error_1e97d97a923eaddd810e056c828e99ea'] = 'Payment error';
+$_MODULE['<{byjuno}prestashop>payment_error_c74e2d6724e2edc933a7bded86301b08'] = 'You are not allowed to pay with this payment method. Please try different.';
+$_MODULE['<{byjuno}prestashop>payment_error_569fd05bdafa1712c4f6be5b153b8418'] = 'Other payment methods';
+
+$_MODULE['<{byjuno}prestashop>byjuno_ba3bbb8c09357b3430e21c1511a49f5f'] = 'CembraPay invoice';
+$_MODULE['<{byjuno}prestashop>byjuno_d99dbdeb4e35b0cac27448595b09c1c3'] = 'CembraPay single invoice';
+$_MODULE['<{byjuno}prestashop>byjuno_dbd37025b503190124596363e32f3c2e'] = 'CembraPay ZERO 3in3';
+$_MODULE['<{byjuno}prestashop>byjuno_4147ef096a0a067a1b3a6ccd9758a0d6'] = 'CembraPay ZERO 4in4';
+$_MODULE['<{byjuno}prestashop>byjuno_40c5acd4838e7b876fc335a9ee81a837'] = 'CembraPay ZERO 6in6';
+$_MODULE['<{byjuno}prestashop>byjuno_9ed230c74799a3db4bdc4294b42080bd'] = 'CembraPay ZERO 12in12';
+$_MODULE['<{byjuno}prestashop>byjuno_ef5010d8911ba4b1a4d2665c22a8e51c'] = 'CembraPay ZERO 24in24';
+$_MODULE['<{byjuno}prestashop>byjuno_3f4cb51dc3d57775b11eead1fd42578c'] = 'CembraPay ZERO 36in36';
+$_MODULE['<{byjuno}prestashop>byjuno_d5a775f16832f9f1933440899be639d1'] = 'CembraPay ZERO 48in48';
+
+$_MODULE['<{byjuno}prestashop>byjuno_06aac5c4279d0d93b5527804b88827e1'] = 'CembraPay Invoice';
+$_MODULE['<{byjuno}prestashop>byjuno_b7d009d8f8d9c3d5e07692546dcf3806'] = 'CembraPay Installment';
+
+$_MODULE['<{byjuno}prestashop>byjuno_962c29e9d818d87968b7460bf9a6472b'] = 'Select payment';
+
+$_MODULE['<{byjuno}prestashop>byjuno_879f6b8877752685a966564d072f498f'] = 'Your shopping cart is empty.';
+$_MODULE['<{byjuno}prestashop>byjuno_de373d72d209506f178c46735ecbf447'] = 'Select payment plan';
+$_MODULE['<{byjuno}prestashop>byjuno_eaf1849876a6548ecc5b804d7da44b27'] = 'Receive invoice';
+$_MODULE['<{byjuno}prestashop>byjuno_dfc27ef27b631b0e7114573a439c3180'] = 'by e-mail';
+$_MODULE['<{byjuno}prestashop>byjuno_3cd02748d4e2c00844a284b71a479ba1'] = 'dispatch in paper form by post (for a fee of CHF 4.25)';
+$_MODULE['<{byjuno}prestashop>byjuno_019ec3132cdf8ee0f2e2a75cf5d3e459'] = 'Salutation';
+$_MODULE['<{byjuno}prestashop>byjuno_63889cfb9d3cbe05d1bd2be5cc9953fd'] = 'Mr.';
+$_MODULE['<{byjuno}prestashop>byjuno_b719ce180ec7bd9641fece2f920f4817'] = 'Ms.';
+$_MODULE['<{byjuno}prestashop>byjuno_10803b83a68db8f7e7a33e3b41e184d0'] = 'Date of Birth';
+$_MODULE['<{byjuno}prestashop>byjuno_569fd05bdafa1712c4f6be5b153b8418'] = 'Other payment methods';
+$_MODULE['<{byjuno}prestashop>byjuno_46b9e3665f187c739c55983f757ccda0'] = 'I confirm my order';
+$_MODULE['<{byjuno}prestashop>byjuno_a6d6486d3d26f3ee5d215b120b4d9aa9'] = 'I have read and accept the {1}Terms and Conditions{2} and the {3}Data Privacy Policy{4} of CembraPay.';
+$_MODULE['<{byjuno}prestashop>byjuno_ebd29ea4b1bd3dc92af6104cb4efa135'] = 'You must agree with terms and conditions';
+$_MODULE['<{byjuno}prestashop>byjuno_0d07446aa5a3ae551bda96e7dadcd0cd'] = 'https://cembrapay.ch/en/terms';
+$_MODULE['<{byjuno}prestashop>byjuno_51ac4ff7f71235c19d06298e3a513536'] = 'https://cembrapay.ch/en/privacy';
+
+$_MODULE['<{byjuno}prestashop>byjuno_5ee8c79dfc349c62050783e00349ef76'] = 'CembraPay payment gateway';
+$_MODULE['<{byjuno}prestashop>byjuno_fcc612d49d54a345ef52a746981a6c08'] = 'CembraPay';
+$_MODULE['<{byjuno}prestashop>validation_e2b7dec8fa4b498156dfee6e4c84b156'] = 'This payment method is not available.';
+
+return $_MODULE;
