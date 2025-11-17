dc1e7ae241ffe1f701c4266089fc047d48dfc4e6
byjuno-dev <byjuno-dev@byjuno.ch>
2025-11-17 10:55:13 +0200

    fix upgrade

--- upgrade/upgrade-2.0.0.php
+++ upgrade/upgrade-2.0.0.php
@@ -27,10 +27,11 @@ function upgrade_module_2_0_0($module)
                   `creation_date` TIMESTAMP NULL DEFAULT now() ,
                   PRIMARY KEY  (`cembra_id`)
                 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;');
-    $defaultStateId = $module->addOrderState("Awaiting for CembraPay");
-    $receivedPaymentId = $module->addOrderState("CembraPay payment success", "#32CD32", true, true);
-    $s4FailId = $module->addOrderState("CembraPay settle fail", "#FF0000", true, true);
-    $s5FailId = $module->addOrderState("CembraPay refund fail", "#FF0000", true, true);
+
+    $defaultStateId = $module->addOrderState("Awaiting for CembraPay", false);
+    $receivedPaymentId = $module->addOrderState("CembraPay payment success", true, "#32CD32", true, true);
+    $s4FailId = $module->addOrderState("CembraPay settle fail", false, "#FF0000", true, true);
+    $s5FailId = $module->addOrderState("CembraPay refund fail", false, "#FF0000", true, true);
     Configuration::updateValue('CEMBRA_ORDER_STATE_DEFAULT', $defaultStateId);
     Configuration::updateValue('CEMBRA_ORDER_STATE_COMPLETE', $receivedPaymentId);
     Configuration::updateValue('CEMBRA_ORDER_S4_FAIL', $s4FailId);
